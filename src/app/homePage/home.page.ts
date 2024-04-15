d0c242dfce5f1764147efaba0f0e1f0610cd2a3e
Andre Schubert <andre.schubert@notyourtypical.dev>
2024-04-15 18:16:53 +1000

    final touch 2

--- src/app/homePage/home.page.ts
+++ src/app/homePage/home.page.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonTitle, IonToolbar, IonContent, IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
+import { IonHeader, IonTitle, IonToolbar, IonContent, IonGrid, IonRow, IonCol, IonButton, IonButtons, IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { helpCircleOutline } from 'ionicons/icons';
+import { AlertService } from '../services/alert.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonCol, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow],
+  imports: [IonIcon, IonButton, IonCol, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonButtons],
 })
 export class HomePage {
 
-  constructor() { }
+  constructor(private alertService: AlertService) {
+    addIcons({ helpCircleOutline })
+  }
 
+  openHelp() {
+    this.alertService.renderAlert('Help', 'This page only holds basic information. Click on the "All Artists" tab to view all artists. Click on the "Search Artists" tab to search for artists. Click on the "Add Artist" tab to add a new artist.');
+  }
 }
